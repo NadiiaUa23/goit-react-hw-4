4bc7d26eaf60554818fac0b29e77c21c71a95196
NadiiaUa23 <hasiminka92@gmail.com>
2024-10-22 20:13:05 +0200

    библиотека axios, запрос к  Image Gallery через HTTP

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,21 @@
+const SearchBar = () => {
+return (
+    <>
+ 
+  <form>
+    <input
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+    <button type="submit">Search</button>
+  </form>
+
+
+    </>
+)
+
+};
+
+export default SearchBar;
