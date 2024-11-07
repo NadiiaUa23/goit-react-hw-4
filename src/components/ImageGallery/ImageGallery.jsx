8817210ed8fddb01b9d5303cf280b55227384110
NadiiaUa23 <hasiminka92@gmail.com>
2024-11-07 23:34:35 +0100

    добавила частковій css

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,12 +1,12 @@
 import ImageCard from "../ImageCaard/ImageCard";
-// import css from "./ImageGallery.module.css";
+import css from "./ImageGallery.module.css";
 
 const ImageGallery = ({ photos }) => {
   //приймає масив images як проп, і для кожного зображення створює елемент li з компонентом ImageCard.
   return (
-    <ul>
+    <ul className={css.list}>
       {photos.map((photo) => (
-        <li key={photo.id}>
+        <li key={photo.id} className={css.listImg}>
           <ImageCard src={photo.urls.thumb} alt={photo.alt_description} />
         </li>
       ))}
